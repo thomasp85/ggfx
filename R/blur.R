7c3e376f588735b6aefe889c51f0fbc48853aa44
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-11-29 11:07:06 +0100

    Add documentation

--- R/blur.R
+++ R/blur.R
@@ -1,7 +1,29 @@
+#' Apply a gaussian blur to your layer
+#'
+#' This filter adds a blur to the provided ggplot layer. The amount of blur can
+#' be controlled and the result can optionally be put underneath the original
+#' layer.
+#'
+#' @param layer A ggplot2 layer object, usually constructed with a call to
+#' `geom_*()` or `stat_*()`
+#' @param sigma The standard deviation of the gaussian kernel. Increase it to
+#' apply more blurring
+#' @param radius The radius of the blur. If `0` it will be calculated
+#' automatically from `sigma`
+#' @param stack Should the original layer be placed on top? Defaults to `FALSE`
+#'
+#' @return A modified `Layer` object
+#'
 #' @importFrom ggplot2 ggproto
 #' @importFrom grid gTree
 #' @export
-with_blur <- function(layer, radius = 0, sigma = 0.5, stack = FALSE) {
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(mtcars, aes(mpg, disp)) +
+#'   with_blur(geom_point(data = mtcars[4,], size = 3), sigma = 3)
+#'
+with_blur <- function(layer, sigma = 0.5, radius = 0, stack = FALSE) {
   parent_geom <- layer$geom
   blur_layer <- ggproto(NULL, layer,
     geom = ggproto('BlurredGeom', parent_geom,
