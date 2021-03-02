978e42c9f2f6200767209be3c9014b7b9f4bc79a
Thomas Lin Pedersen <thomasp85@gmail.com>
2021-03-02 16:38:24 +0100

    Add additional raster helpers

--- R/raster-helpers.R
+++ R/raster-helpers.R
@@ -33,9 +33,55 @@ NULL
 viewport_location <- function() {
   bbox <- c(unlist(deviceLoc(unit(0, 'npc'), unit(0, 'npc'), TRUE)),
             unlist(deviceLoc(unit(1, 'npc'), unit(1, 'npc'), TRUE)))
-  bbox <- round(bbox * current_resolution())
+  bbox <- bbox * current_resolution()
+  height <- dev.size('px')[2]
+  bbox[c(2, 4)] <- height - bbox[c(4, 2)]
+  bbox <- as.integer(round(bbox))
   names(bbox) <- c('xmin', 'ymin', 'xmax', 'ymax')
   bbox
+}
+
+#' @rdname render_context
+#' @export
+index_raster <- function(raster, cols, rows) {
+  dims <- dim(raster)
+  cells <- expand.grid(x = cols, y = rows)
+  index <- (cells$y - 1) * dims[2] + cells$x
+  area <- .subset(raster, index)
+  class(area) <- class(raster)
+  dim(area) <- c(length(rows), length(cols))
+  area
+}
+
+#' @rdname render_context
+#' @export
+get_raster_area <- function(raster, xmin, ymin, xmax, ymax) {
+  index_raster(raster, seq(xmin, xmax), seq(ymin, ymax))
+}
+
+#' @rdname render_context
+#' @export
+set_raster_area <- function(raster, value, xmin, ymin) {
+  value_dim <- dim(value)
+  cells <- expand.grid(x = seq(xmin, xmin + value_dim[2] - 1), y = seq(ymin, ymin + value_dim[1] - 1))
+  dims <- dim(raster)
+  index <- (cells$y - 1) * dims[2] + cells$x
+  raster[index] <- as.integer(value)
+  raster
+}
+
+#' @rdname render_context
+#' @export
+get_viewport_area <- function(raster) {
+  loc <- viewport_location()
+  get_raster_area(raster, loc[1], loc[2], loc[3], loc[4])
+}
+
+#' @rdname render_context
+#' @export
+set_viewport_area <- function(raster, value) {
+  loc <- viewport_location()
+  set_raster_area(raster, value, loc[1], loc[2])
 }
 
 #' @rdname render_context
@@ -53,14 +99,28 @@ current_resolution <- function() {
 }
 
 #' @rdname render_context
-#' @importFrom grid is.unit
+#'
+#' @param y_axis is the unit pertaining to the y-axis? Defaults to `FALSE` (i.e.
+#' it is measured on the x-axis)
+#' @param location is the unit encoding a location? Defaults to `FALSE` (i.e. it
+#' is encoding a dimension). Pixel locations are encoded based on a top-left
+#' starting point, as opposed to grid's bottom-left coordinate system. This
+#' means that y-axis locations will flip around when converted to pixels.
+#'
+#' @importFrom grid is.unit convertWidth convertHeight convertX convertY
 #' @export
-as_pixels <- function(x) {
+as_pixels <- function(x, y_axis = FALSE, location = FALSE) {
   if (is.unit(x)) {
-    x <- convertWidth(x, 'inch', valueOnly = TRUE)
+    mode <- y_axis + location * 2 + 1
+    x <- switch(mode,
+      convertWidth(x, 'inch', valueOnly = TRUE),              # FALSE FALSE
+      convertHeight(x, 'inch', valueOnly = TRUE),             # TRUE  FALSE
+      deviceLoc(x, x, valueOnly = TRUE)$x,                    # FALSE TRUE
+      dev.size('in')[2] - deviceLoc(x, x, valueOnly = TRUE)$y # TRUE  TRUE
+    )
     x <- x * current_resolution()
   }
-  x
+  as.integer(round(x))
 }
 
 #' @rdname render_context
